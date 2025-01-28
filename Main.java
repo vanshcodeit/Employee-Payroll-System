2e6bfe16305f67082f5b8cc1b2d6c89a290f938f
vanshcodeit <vanshnegi20@gmail.com>
2025-01-28 16:29:08 +0530

    creating classes of partime and fulltime employee

--- Main.java
+++ Main.java
@@ -1,3 +1,17 @@
+abstract class Employee{
+    
+    private String name;
+
+    private int id;
+
+
+
+}
+
 public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+        
+    }
 
 }
